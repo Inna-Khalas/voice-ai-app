17698ba745ca1dee624fe5f82e6f206c7bfa0121
Inka <innamakarec349@gmail.com>
2025-07-08 01:47:23 +0400

    delete {}

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+
+  {
+    files: ['src/generated/prisma/**'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
