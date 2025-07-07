21cdfcf0b15b09fa395ad94e9e5ebb39e9063523
Inka <innamakarec349@gmail.com>
2025-07-08 01:50:01 +0400

    delete {}

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
   {
     files: ['src/generated/prisma/**'],
     rules: {
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
